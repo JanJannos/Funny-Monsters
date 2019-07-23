513a07a1d1671a64cbd4d82b79d1b52468033b2c
unknown <hanohj@gmail.com>
2019-07-23 07:08:55 +0300

    Updates

---
+++ precache-manifest.041b20a5d9e67fd07f06af52c7bb5ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e6cbe5c0c84cbbf75d2c0125bdacde",
+    "url": "/Funny-Monsters/index.html"
+  },
+  {
+    "revision": "48bb1e9a29a75a59c3e3",
+    "url": "/Funny-Monsters/static/css/main.9c165cf8.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/Funny-Monsters/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "48bb1e9a29a75a59c3e3",
+    "url": "/Funny-Monsters/static/js/main.63bb71ad.chunk.js"
+  },
+  {
+    "revision": "794fa249d50ebd0cddf0",
+    "url": "/Funny-Monsters/static/js/runtime~main.649906c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Funny-Monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
